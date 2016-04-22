7fb66479fbc84b788264b7e48983037040f36379
Alex Watanabe <alexcw@hawaii.edu>
2016-04-22 11:54:14 -1000

    Write templateString

--- alexcw_mad_libs/madlibs.php
+++ alexcw_mad_libs/madlibs.php
@@ -1,18 +1,32 @@
 <?php
 
-$templateString = "Hi I'm name and noun verb adjective!";
 
+$templateString = "Hi I'm ++name++! ++noun2++ is ++adjective1++
+and ++noun1++ is ++adjective2++! ++noun2++ ++verb2++ the ++adjective3++
+++noun3++! The end, also ++verb1++!";
 
-
-
-foreach($_GET as $key => $value)
+if ($_GET == NULL)
 {
 
-  $templateString = str_replace($key,$value,$templateString);
+  $templateString = "";
 
 }
+else {
+  foreach($_GET as $key => $value)
+  {
 
-echo $templateString;
+    if ($value == NULL)
+    {
+    $templateString = str_replace($key,"________",$templateString);
+    }
+    else
+    {
+    $templateString = str_replace($key,$value,$templateString);
+    }
+  }
+}
+
+
 
 
  ?>
@@ -25,25 +39,35 @@ echo $templateString;
           Please fill out the boxes with the requested information:
           <table>
             <tr>
-              <td>Your Name:</td>
-              <td><input type="text" name="name"></td>
+              <td>Name:</td>
+              <td><input type="text" name="++name++" value=""></td>
             </tr>
             <tr>
-              <td>A Noun:</td>
-              <td><input type="text" name="noun"></td>
+              <td>Noun:</td>
+              <td><input type="text" name="++noun1++" value=""></td>
+              <td>Noun:</td>
+              <td><input type="text" name="++noun2++" value=""></td>
+              <td>Noun:</td>
+              <td><input type="text" name="++noun3++" value=""></td>
             </tr>
             <tr>
-              <td>A Verb:</td>
-              <td><input type="text" name="verb"></td>
+              <td>Verb:</td>
+              <td><input type="text" name="++verb1++" value=""></td>
+              <td>Verb:</td>
+              <td><input type="text" name="++verb2++" value=""></td>
             </tr>
             <tr>
-              <td>An Adjective:</td>
-              <td><input type="text" name="adjective"></td>
+              <td>Adjective:</td>
+              <td><input type="text" name="++adjective1++" value=""></td>
+              <td>Adjective:</td>
+              <td><input type="text" name="++adjective2++" value=""></td>
+              <td>Adjective:</td>
+              <td><input type="text" name="++adjective3++" value=""></td>
             </tr>
 
           </table>
 
-        <input type="submit">  <? echo $error; ?>
+        <input type="submit">  <? echo $templateString; ?>
 
         </fieldset>
     </form>
